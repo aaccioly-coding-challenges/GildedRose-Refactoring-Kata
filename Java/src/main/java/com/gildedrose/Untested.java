70c53f5ec478c2450c4ffa323d8268fdc6b5d11f
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2024-07-06 02:57:14 +0100

    Add one more uncovered line to test

--- Java/src/main/java/com/gildedrose/Untested.java
+++ Java/src/main/java/com/gildedrose/Untested.java
@@ -4,5 +4,6 @@ public class Untested {
 
     public void untestedMethod() {
         System.out.println("This method is untested");
+        System.out.println("It is not called from any test");
     }
 }
