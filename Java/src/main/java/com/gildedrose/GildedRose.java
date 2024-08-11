918e4c67b10d7c9ed77cdf4ec9a2b1c98b8a2a4f
O. Link <link1@uni-potsdam.de>
2024-08-11 16:21:46 +0200

    Change update item to use is "backstage pass"-classifier

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -45,7 +45,7 @@ class GildedRose {
             return;
         }
 
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!isBackStagePass(item.name)) {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
@@ -53,13 +53,10 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
-
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
@@ -74,7 +71,7 @@ class GildedRose {
 
         if (item.sellIn < 0) {
             // normal items -> quality decreases twice as fast after expiration date
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!isBackStagePass(item.name)) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
